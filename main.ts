fdebc4fec14093988731cf47fd05f0683955c307
tangperry <87504139+tangperry@users.noreply.github.com>
2022-02-17 14:09:20 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,27 @@
+// basic.forever(function () {
+// basic.showLeds(`
+// . . # . .
+// . # . # .
+// # # # # #
+// # . . . #
+// . . . . .
+// `)
+// basic.showLeds(`
+// # . . . #
+// # . . . #
+// . # . # .
+// . . # . .
+// . . . . .
+// `)
+// basic.showNumber(0)
+// })
+// basic.forever(function () {
+// basic.showString("123")
+// })
+
+
+basic.forever(function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showString("U2FsdGVk12KUAdl9cJZ3dqZoluUhzYFw")
+    }
+})
